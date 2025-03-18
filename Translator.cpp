96db09a9293bb9ad233c5bf0d534256cafe4adc3
Dylan Sullins <sullins.dylan@gmail.com>
2025-03-18 16:48:47 -0500

    Translator part one complete, tested against nand2tetris test files

--- Translator.cpp
+++ Translator.cpp
@@ -6,20 +6,24 @@
 #include <numeric>
 #include <unordered_map>
 
-std::unordered_map<std::string, std::string> SymbolTable = 
-{
-    {"local", "LC"},
-    {"argument", "ARG"},
-    {"this", "THIS"},
-    {"that", "THAT"},
-    {"screen", "SCREEN"},
-    {"keyboard", "KBD"},
-};
+static int eqCount = 0;
+static int endCount = 0;
+static int gtCount = 0;
+static int ltCount = 0;
+
+
 
 std::unordered_map<std::string, std::string> CommandTable =
 {
     {"add", "@SP\nAM=M-1\nD=M\nA=A-1\nM=D+M\n"},
     {"sub", "@SP\nAM=M-1\nD=M\nA=A-1\nM=M-D\n"},
+    {"neg", "@SP\nA=M-1\nM=-M\n"},
+    {"eq",  "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@EQUAL_"+(std::to_string(eqCount++))+"\nD;JEQ\n@SP\nA=M-1\nM=0\n@END"+(std::to_string(endCount++))+"\n0;JMP\n(EQUAL)\n@SP\nA=M-1\nM=-1\n(END)"},
+    {"gt",  "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@GT\nD;JGT\n@SP\nA=M-1\nM=0\n@END\n0;JMP\n(GT)\n@SP\nA=M-1\nM=-1\n(END)"},
+    {"lt",  "\n"},
+    {"and", "\n"},
+    {"or",  "\n"},
+    {"not", "\n"},
 };
 
 std::string RemoveWhiteSpaceAndComments(std::string line)
@@ -36,7 +40,7 @@ std::string RemoveWhiteSpaceAndComments(std::string line)
     return decommentedLine;
 }
 
-std::string TranslateLine(std::string line)
+std::string TranslateLine(std::string line, std::string filename)
 {
     std::string commentedOrigLine = "// " + line + '\n';
     std::stringstream inSS(line);
@@ -44,47 +48,131 @@ std::string TranslateLine(std::string line)
     std::vector<std::string> tokens;
     std::vector<std::string> newLine;
     newLine.push_back(commentedOrigLine);
-    while(inSS.good())
+    while(inSS >> substr)
     {
-        std::getline(inSS, substr, ' ');
         tokens.push_back(substr);
     }
     
     if (tokens.size() == 3 )
     {
+        std::string command = tokens[0];
+        std::string keyword = tokens[1];
+        std::string value = tokens[2];
 
-        if (tokens[1] == "constant")
+        std::string segment =   (keyword == "local")    ? "LCL"     :
+                                (keyword == "argument") ? "ARG"     :
+                                (keyword == "this")     ? "THIS"    : 
+                                (keyword == "that")     ? "THAT"    :
+                                (keyword == "screen")   ? "SCREEN"  :
+                                (keyword == "keyboard") ? "KBD"     : "OTHER";
+        std::string ptrAddress = (std::stoi(value) == 0) ? "3" : "4";
+
+        if (command == "push")
         {
-            newLine.push_back("@"+tokens[2]+'\n'+"D=A\n");
-        }
-        else if (tokens[1] == "temp")
-        {
-            int addr = 5 + std::stoi(tokens[2]);
-            newLine.push_back("@"+std::to_string(addr)+'\n');
-        }
-        else
-        {
-            if (SymbolTable.find(tokens[1]) != SymbolTable.end()) newLine.push_back("@"+SymbolTable[tokens[1]]+'\n'+"D=M\n");
-            else std::cout << "ERROR: SYMBOL TOKEN NOT FOUND";
-        }
-        if (tokens[0] == "push")
-        {
+            if (keyword == "constant")
+            {
+                newLine.push_back("@"+value+'\n'+"D=A\n");
+            }
+            else if (keyword == "temp")
+            {
+                int addr = 5 + std::stoi(value);
+                newLine.push_back("@"+std::to_string(addr)+"\nD=M\n");
+            }
+            else if (keyword == "local" || keyword == "argument" || keyword == "this" || keyword == "that")
+            {
+
+                newLine.push_back("@"+segment+"\nD=M\n@"+value+"\nA=D+A\nD=M\n");
+            }
+            else if (keyword == "pointer")
+            {
+                newLine.push_back("@"+ptrAddress+"\nD=M\n");
+            }
+            else if (keyword == "static")
+            {
+                std::string staticVar = filename + "." + value;
+                newLine.push_back("@"+staticVar+"\nD=M\n");
+            }
             newLine.push_back("@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-        } 
-        else if (tokens[0] == "pop")
+        }
+        else if (command == "pop")
         {
-            newLine.push_back("@SP\nAM=M-1\nD=M\n");
+            if (keyword == "local" || keyword == "this" || keyword == "that" || keyword == "argument")
+            {  
+                newLine.push_back("@"+segment+"\nD=M\n@"+value+"\nD=D+A\n@R13\nM=D\n");
+                newLine.push_back("@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n");
+            }
+            else if (keyword == "temp")
+            {
+                newLine.push_back("@SP\nAM=M-1\nD=M\n");
+                int addr = 5 + std::stoi(value);
+                newLine.push_back("@"+std::to_string(addr)+"\nM=D\n");
+            }
+            else if (keyword == "pointer")
+            {
+                newLine.push_back("@SP\nAM=M-1\nD=M\n@"+ptrAddress+"\nM=D\n");
+            }
+            else if (keyword == "static")
+            {
+                std::string staticVar = filename + "." + value;
+                newLine.push_back("@SP\nAM=M-1\nD=M\n@"+staticVar+"\nM=D\n");
+            }
         }
     }
     else if (tokens.size() == 1)
     {
-        if (CommandTable.find(tokens[0]) != CommandTable.end()) newLine.push_back(CommandTable.at(tokens[0]));
-        else std::cout << "ERROR: COMMAND TOKEN NOT FOUND";
+        std::string command = tokens[0];
+        if (command == "add")
+        {
+            newLine.push_back("@SP\nAM=M-1\nD=M\nA=A-1\nM=D+M\n");
+        }
+        else if (command == "sub")
+        {
+            newLine.push_back("@SP\nAM=M-1\nD=M\nA=A-1\nM=M-D\n");
+        }
+        else if (command == "neg")
+        {
+            newLine.push_back("@SP\nA=M-1\nM=-M\n");
+        }
+        else if (command == "eq")
+        {
+            std::string eqLabel = "EQ_" + std::to_string(eqCount++);
+            std::string endLabel = "END_" + std::to_string(endCount++);
+            newLine.push_back("@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@"+eqLabel+"\nD;JEQ\n@SP\nA=M-1\nM=0\n@"+endLabel+"\n0;JMP\n("+eqLabel+")\n@SP\nA=M-1\nM=-1\n("+endLabel+")\n");
+        }
+        else if (command == "gt")
+        {
+            std::string gtLabel = "GT_" + std::to_string(eqCount++);
+            std::string endLabel = "END_" + std::to_string(endCount++);
+            newLine.push_back("@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@"+gtLabel+"\nD;JGT\n@SP\nA=M-1\nM=0\n@"+endLabel+"\n0;JMP\n("+gtLabel+")\n@SP\nA=M-1\nM=-1\n("+endLabel+")\n");
+        }
+        else if (command == "lt")
+        {
+            std::string ltLabel = "LT_" + std::to_string(eqCount++);
+            std::string endLabel = "END_" + std::to_string(endCount++);
+            newLine.push_back("@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@"+ltLabel+"\nD;JLT\n@SP\nA=M-1\nM=0\n@"+endLabel+"\n0;JMP\n("+ltLabel+")\n@SP\nA=M-1\nM=-1\n("+endLabel+")\n");
+        }
+        else if (command == "and")
+        {
+            newLine.push_back("@SP\nAM=M-1\nD=M\nA=A-1\nM=D&M\n");
+        }
+        else if (command == "or")
+        {
+            newLine.push_back("@SP\nAM=M-1\nD=M\nA=A-1\nM=D|M\n");
+        }
+        else if (command == "not")
+        {
+            newLine.push_back("@SP\nA=M-1\nM=!M\n");
+        }
+        else 
+        {
+            std::cout << "ERROR: COMMAND TOKEN NOT FOUND";
+            return "";
+        }
     }
     for (int i = 0; i < newLine.size(); ++i) {
         translatedLine += newLine.at(i);
     }
-    return std::accumulate(newLine.begin(), newLine.end(), std::string{});
+    return translatedLine;
 }
 
 int main(int argc, char* argv[])
@@ -94,9 +182,14 @@ int main(int argc, char* argv[])
     filenameIn = argv[1];
     filenameOut = argv[2];
 
-    std::ifstream fin(argv[1]);
+    std::ifstream fin(filenameIn);
     std::ofstream fout(filenameOut, std::ofstream::out | std::ofstream::binary);
-
+    int directoryPos = filenameIn.find_last_of('\\');
+    if (directoryPos == -1) directoryPos = 0;
+    int extensionPos = filenameIn.find_last_of('.');
+    if (extensionPos == -1) return -1;
+    std::string filename = filenameIn.substr(directoryPos + 1, extensionPos - directoryPos - 1);
+    std::cout << "Beginning translation of file '" << filenameIn << "' to Hack Assembly file '" << filenameOut << "'" << std::endl;
     std::string line;
     while (std::getline(fin, line))
     {
@@ -105,7 +198,8 @@ int main(int argc, char* argv[])
         {
             continue;
         }
-        std::string assemblyLine = TranslateLine(line);
+        std::string assemblyLine = TranslateLine(line, filename);
         fout << assemblyLine;
     }
+    std::cout << "Translation Complete! " << filenameOut << " Created!" << std::endl;
 }
