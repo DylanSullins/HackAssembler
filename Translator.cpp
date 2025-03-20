8194f0cb6d54405adfde450e241142f2c3bb1aed
Dylan Sullins <sullins.dylan@gmail.com>
2025-03-19 19:32:38 -0500

    Added function, call, and return functionality

--- Translator.cpp
+++ Translator.cpp
@@ -5,26 +5,17 @@
 #include <vector>
 #include <numeric>
 #include <unordered_map>
+#include <filesystem>
 
 static int eqCount = 0;
 static int endCount = 0;
 static int gtCount = 0;
 static int ltCount = 0;
+static int retCount = 0;
 
 
 
-std::unordered_map<std::string, std::string> CommandTable =
-{
-    {"add", "@SP\nAM=M-1\nD=M\nA=A-1\nM=D+M\n"},
-    {"sub", "@SP\nAM=M-1\nD=M\nA=A-1\nM=M-D\n"},
-    {"neg", "@SP\nA=M-1\nM=-M\n"},
-    {"eq",  "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@EQUAL_"+(std::to_string(eqCount++))+"\nD;JEQ\n@SP\nA=M-1\nM=0\n@END"+(std::to_string(endCount++))+"\n0;JMP\n(EQUAL)\n@SP\nA=M-1\nM=-1\n(END)"},
-    {"gt",  "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@GT\nD;JGT\n@SP\nA=M-1\nM=0\n@END\n0;JMP\n(GT)\n@SP\nA=M-1\nM=-1\n(END)"},
-    {"lt",  "\n"},
-    {"and", "\n"},
-    {"or",  "\n"},
-    {"not", "\n"},
-};
+
 
 std::string RemoveWhiteSpaceAndComments(std::string line)
 {
@@ -117,6 +108,43 @@ std::string TranslateLine(std::string line, std::string filename)
                 newLine.push_back("@SP\nAM=M-1\nD=M\n@"+staticVar+"\nM=D\n");
             }
         }
+        else if (command == "function")
+        {
+            newLine.push_back("("+keyword+")\n");
+            for (int i=0; i<std::stoi(value); ++i)
+            {
+                newLine.push_back("@SP\nA=M\nM=0\n@SP\nM=M+1\n");
+            }
+        }
+        else if (command == "call")
+        {
+            std::string retLabel = keyword + "$ret." + std::to_string(retCount++);
+            newLine.push_back("@"+retLabel+"\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"); // Return label
+            newLine.push_back("@LCL\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+            newLine.push_back("@ARG\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+            newLine.push_back("@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+            newLine.push_back("@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+            newLine.push_back("@SP\nD=M\n@5\nD=D-A\n@"+value+"\nD=D-A\n@ARG\nM=D\n");
+            newLine.push_back("@SP\nD=M\n@LCL\nM=D\n");
+            newLine.push_back("@"+keyword+"\n0;JMP\n("+retLabel+")\n");
+        }
+    }
+    else if (tokens.size() == 2)
+    {
+        std::string command = tokens[0];
+        std::string label = tokens[1];
+        if (command == "label")
+        {
+            newLine.push_back("("+label+")\n");
+        }
+        else if (command == "goto")
+        {
+            newLine.push_back("@"+label+"\n0;JMP\n");
+        }
+        else if (command == "if-goto")
+        {
+            newLine.push_back("@SP\nAM=M-1\nD=M\n@"+label+"\nD;JNE\n");
+        }
     }
     else if (tokens.size() == 1)
     {
@@ -163,6 +191,19 @@ std::string TranslateLine(std::string line, std::string filename)
         {
             newLine.push_back("@SP\nA=M-1\nM=!M\n");
         }
+        else if (command == "return")
+        {
+            newLine.push_back("@LCL\nD=M\n@R13\nM=D\n");
+            newLine.push_back("@5\nA=D-A\nD=M\n@R14\nM=D\n");
+            newLine.push_back("@SP\nAM=M-1\nD=M\n@ARG\nA=M\nM=D\n");
+            newLine.push_back("@ARG\nD=M+1\n@SP\nM=D\n");
+            newLine.push_back("@R13\nAM=M-1\nD=M\n@THAT\nM=D\n");
+            newLine.push_back("@R13\nAM=M-1\nD=M\n@THIS\nM=D\n");
+            newLine.push_back("@R13\nAM=M-1\nD=M\n@ARG\nM=D\n");
+            newLine.push_back("@R13\nAM=M-1\nD=M\n@LCL\nM=D\n");
+            newLine.push_back("@R14\nA=M\n0;JMP\n");
+            
+        }
         else 
         {
             std::cout << "ERROR: COMMAND TOKEN NOT FOUND";
@@ -177,29 +218,63 @@ std::string TranslateLine(std::string line, std::string filename)
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3) return -1;
-    std::string filenameIn, filenameOut;
-    filenameIn = argv[1];
-    filenameOut = argv[2];
-
-    std::ifstream fin(filenameIn);
-    std::ofstream fout(filenameOut, std::ofstream::out | std::ofstream::binary);
-    int directoryPos = filenameIn.find_last_of('\\');
-    if (directoryPos == -1) directoryPos = 0;
-    int extensionPos = filenameIn.find_last_of('.');
-    if (extensionPos == -1) return -1;
-    std::string filename = filenameIn.substr(directoryPos + 1, extensionPos - directoryPos - 1);
-    std::cout << "Beginning translation of file '" << filenameIn << "' to Hack Assembly file '" << filenameOut << "'" << std::endl;
-    std::string line;
-    while (std::getline(fin, line))
-    {
-        line = RemoveWhiteSpaceAndComments(line);
-        if (line == "" || line == " ")
+    if (argc != 2) return -1;
+    std::string inputPath = argv[1];
+    std::string outputPath;
+    std::vector<std::string> vmFiles;
+    if (std::filesystem::is_directory(inputPath)){
+        outputPath = inputPath + std::filesystem::path(inputPath).stem().string() + ".asm";
+        for (const auto& entry : std::filesystem::directory_iterator(inputPath))
         {
-            continue;
+            if (entry.path().extension() == ".vm")
+            {
+                vmFiles.push_back(entry.path().string());
+            }
         }
-        std::string assemblyLine = TranslateLine(line, filename);
-        fout << assemblyLine;
     }
-    std::cout << "Translation Complete! " << filenameOut << " Created!" << std::endl;
+    else if (std::filesystem::is_regular_file(inputPath) && std::filesystem::path(inputPath).extension() == ".vm")
+    {
+        vmFiles.push_back(inputPath);
+        outputPath = std::filesystem::path(inputPath).replace_extension(".asm").string();
+    }
+    else 
+    {
+        std::cerr << "Invalid input: Must be a .vm file or a directory containing .vm files.\n";
+        return -1;
+    }
+    std::cout << outputPath << std::endl;
+    std::ofstream fout(outputPath, std::ofstream::out | std::ofstream::binary);
+
+    if (vmFiles.size() > 1)
+    {
+        fout    << "// Bootstrap Code\n"
+                << "@256\nD=A\n@SP\nM=D\n"  // SP = 256
+                << "// Call Sys.init\n"
+                << "@Sys.init$ret.0\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                << "@LCL\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                << "@ARG\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                << "@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                << "@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+                << "@SP\nD=M\n@5\nD=D-A\n@ARG\nM=D\n"
+                << "@SP\nD=M\n@LCL\nM=D\n"
+                << "@Sys.init\n0;JMP\n"
+                << "(Sys.init$ret.0)\n";
+    }
+    for (const auto& vmFile : vmFiles)
+    {
+        std::ifstream fin(vmFile);
+        std::string filename = std::filesystem::path(vmFile).stem().string();
+        std::cout << " . . . Processing: " << vmFile << std::endl;
+
+        std::string line;
+        while (std::getline(fin, line)) 
+        {
+            line = RemoveWhiteSpaceAndComments(line);
+            if (line.empty()) continue;
+            std::string assemblyLine = TranslateLine(line, filename);
+            fout << assemblyLine;
+        }
+    }
+    std::cout << "Translation Complete! " << outputPath << " Created!" << std::endl;
+    return 0;
 }
